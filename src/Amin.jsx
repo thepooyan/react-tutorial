7e1c59ec1abf3f826ecd8aba9b338d507be79424
pooyan <thepooyan@gmail.com>
2023-09-02 14:17:15 +0330

    amin created

---
+++ src/Amin.jsx
@@ -0,0 +1,11 @@
+import './Amin.css';
+
+function Amin() {
+    return (
+        <div className='amin'>
+            this is the koskhole amin!
+        </div>
+    )
+}
+
+export default Amin;
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Amin from "./Amin"
+
+function App() {
+  return (
+    <div>
+      <Amin/>
+      <Amin/>
+      <Amin/>
+    </div>
+  )
+}
+
+export default App
