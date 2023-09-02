3c93e848bec7f3fba0e01a84362c4832ab8c773f
pooyan <thepooyan@gmail.com>
2023-09-02 14:29:02 +0330

    next

--- src/Amin.jsx
+++ src/Amin.jsx
@@ -1,10 +1,14 @@
 import './Amin.css';
 
 function Amin(props) {
+
+
+    let a = "name is:" + props;
+
     return (
         <div className='amin'>
             this is the koskhole amin! with color {props.color} -
-            and my weight is : {props.weight}
+            {props.weight? "and my weight is " + props.weight : ""}
         </div>
     )
 }
